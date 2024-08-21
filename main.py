9dd06bab8144890494b7f6b6e9c9835f94627875
Wit Łuczyński <witluczynski@gmail.com>
2024-08-21 21:11:50 +0200

    init

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from flask import Flask, render_template
 import json
+import os
 
-with open("konta_promowane.json", "r", encoding="UTF8") as f:
-    konta_promowane = json.load(f)
+
+konta_bankowe = list()
+
+for path in os.listdir():
+    if path.endswith(".json") and path[:2] == "KB":
+        with open(path, "r", encoding="utf8") as f:
+            konto = json.load(f)
+    
+        konta_bankowe.append(konto)
+
 
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
 
-    return render_template("index.html", konta_promowane=konta_promowane["kontaPromowane"])
+    konta_promowane = [k for k in konta_bankowe if k["kontoPromowane"]]
+
+
+    return render_template("index.html", konta_promowane=konta_promowane)
 
 @app.route("/opis_produktu/<nazwa_konta>")
 def opis_produktu(nazwa_konta):
 
-    return render_template("opis_produktu.html", nazwa_konta = nazwa_konta)
+    opis = [k for k in konta_bankowe if k["nazwa"] == nazwa_konta][0]
+    
+
+    return render_template("opis_produktu.html", nazwa_konta = nazwa_konta, warunki_przystoapienia_do_promocji=opis["warunkiPrzystoapieniaDoPromocji"])
 
 
 if __name__ == "__main__":
